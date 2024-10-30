1b1632d91d2b2348421c059f3ec40bcaa52a7b93
G. Sri Ram Ganesh <161675647+ganeshgovala@users.noreply.github.com>
2024-10-30 15:01:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -226,4 +226,4 @@ def common(wait, driver) :
     return jsonify(message = result)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=5000)
